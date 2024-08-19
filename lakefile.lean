9211c90ced9f080050da824dbc1e3a4948de6a41
Lean 4 VS Code Extension <>
2024-08-19 17:48:34 +0100

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "Imo1982Q3" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Imo1982Q3» where
+  -- add any library configuration options here
